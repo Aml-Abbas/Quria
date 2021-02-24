2c0a639913bdc1981cdf58444a68b482c326a042
Aml-Abbas <aml.abbas@hotmail.com>
2021-02-24 18:43:32 +0100

    .

--- java.js
+++ java.js
@@ -21,8 +21,9 @@ function getIdFromParam() {
  function createLink(itemId) {
     androiduri = "androidrfid://primaryid?itemid=" + itemId;
 
-    getElementById('btn').innerHTML = '<button class="button button5 w3-bar-item w3-button"><a href='+androiduri+'>Write</a></button>;';
-
+    var buttonP = document.getElementById("btn");
+    buttonP.textContent = `<button class="button button5 w3-bar-item w3-button"><a href=` + androiduri+ `>Write</a></button>;`;
+    
   //  var aTag = document.getElementById("link");
    // aTag.setAttribute("href", androiduri);
 } 
